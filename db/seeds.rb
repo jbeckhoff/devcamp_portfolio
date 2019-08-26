3de3a76b801019d74c9b087bac4546542e34836f
John Beckhoff <john.beckhoff@outlook.com>
2019-08-26 12:59:17 -0500

    Integrated index action for portfolio items'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do |blog|
   Blog.create!(
-        title: "My Blog Post #{blog}",
+        title: "My Blog Post #{blog + 1}",
         body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada ornare bibendum. In feugiat turpis neque, sed accumsan nulla rutrum sed. Nam condimentum eros non dictum ornare. Curabitur vel ex tincidunt, feugiat sapien in, lacinia lacus. Proin sollicitudin, libero non lacinia varius, lacus velit pretium tortor, ut pharetra turpis sapien ac nibh. Integer non ante sit amet diam euismod efficitur et vitae lectus. Vestibulum vel fringilla metus. Pellentesque sed pretium felis, ac aliquam ante. Curabitur consequat dictum diam vitae egestas. Donec at luctus mi, vel mollis lorem. Aenean sed ligula eros. Cras accumsan tempor nunc sagittis blandit. Sed tristique eros."
 
     )
@@ -11,7 +11,7 @@ puts "10 Blog posts created"
 
 5.times do |skill|
   Skill.create!(
-    title: "Rails #{skill}",
+    title: "Rails #{skill + 1}",
     percent_utilized: 15
   )
 end
@@ -20,7 +20,7 @@ puts "5 skills created"
 
 9.times do |portfolio_item|
   Portfolio.create!(
-    title: "Portofolio title #{portfolio_item}",
+    title: "Portofolio title #{portfolio_item + 1}",
     subtitle: "My great service",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec tellus aliquam, luctus risus eget, aliquam neque. Nam sollicitudin ex eu fermentum mattis. Nam mi dui, scelerisque at accumsan eget, tempor nec lectus. Phasellus nisi tortor, sodales eu varius sed, placerat nec quam. Nulla lacinia lectus libero, sit amet hendrerit." ,
     main_image: "http://placehold.it/600x400",
