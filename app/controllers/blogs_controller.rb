ad774f2f542d260077263f86ce0a63f5f7cce03c
John Beckhoff <john.beckhoff@outlook.com>
2019-09-01 09:09:30 -0500

    Implemented status toggle(draft?, published?) for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -56,6 +56,15 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    redirect_to blogs_url, notice: 'Blog status has been updated.'
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
@@ -64,6 +73,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :toggle_status)
     end
 end
